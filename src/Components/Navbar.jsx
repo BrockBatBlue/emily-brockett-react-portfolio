48b7aaf7823cbc15a61138e278168b0f65009e3e
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2022-09-19 11:05:52 -0500

    made changes to removed text and content on navbar collapse, navbar does not collapse totally yet

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,76 +3,84 @@ import "../Styles/Navbar.css";
 
 function Navbar() {
     return (
-        <section id="header">
+        <section id="header" className="navbar-expand-sm">
             <div className="container">
-                <div className="col-12">
-                    <header>
-                        <span className="image avatar">
-                            <img
-                                src="images/profile-pic.png"
-                                alt="Emily Brockett"
-                            />
-                        </span>
-                        <h1 id="logo">
-                            <a href="https://github.com/BrockBatBlue">
-                                Emily Brockett
-                            </a>
-                        </h1>
-                        <br></br>
-                        <p>
-                            Currently teaching front-end
-                            <br />
-                            web development
-                        </p>
-                    </header>
-                    <br></br>
-                    <nav id="nav" className="navbar">
-                        <ul>
-                            <li>
-                                <a href="#one" className="active">
-                                    About
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <div className="col-12">
+                        <header>
+                            <span className="image avatar">
+                                <img
+                                    src="images/profile-pic.png"
+                                    alt="Emily Brockett"
+                                />
+                            </span>
+                            <h1 id="logo">
+                                <a href="https://github.com/BrockBatBlue">
+                                    Emily Brockett
                                 </a>
-                            </li>
-                            <li>
-                                <a href="#two">Things I Can Do</a>
-                            </li>
-                            <li>
-                                <a href="#three">A Few Accomplishments</a>
-                            </li>
-                            {/* <li>
+                            </h1>
+                            <br></br>
+                            <p>
+                                Currently teaching front-end
+                                <br />
+                                web development
+                            </p>
+                        </header>
+                        <br></br>
+                        <nav
+                            id="nav"
+                            className="navbar collapse navbar-collapse"
+                        >
+                            <ul>
+                                <li>
+                                    <a href="#one" className="active">
+                                        About
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#two">Things I Can Do</a>
+                                </li>
+                                <li>
+                                    <a href="#three">A Few Accomplishments</a>
+                                </li>
+                                {/* <li>
                                 <a href="#four">Blog</a>
                             </li> */}
-                        </ul>
-                    </nav>
-                    <br></br>
-                    <footer className="icon-footer">
-                        <ul className="icons">
-                            <li>
-                                <a
-                                    href="https://www.linkedin.com/in/emily-brockett/"
-                                    class="icon brands bi-linkedin"
-                                >
-                                    <span className="label"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://github.com/BrockBatBlue"
-                                    className="icon brands bi-github"
-                                >
-                                    <span className="label"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="mailto: emilybrockett@gmail.com"
-                                    class="icon solid bi-envelope"
-                                >
-                                    <span className="label"></span>
-                                </a>
-                            </li>
-                        </ul>
-                    </footer>
+                            </ul>
+                        </nav>
+                        <br></br>
+                        <footer className="icon-footer">
+                            <ul className="icons">
+                                <li>
+                                    <a
+                                        href="https://www.linkedin.com/in/emily-brockett/"
+                                        class="icon brands bi-linkedin"
+                                    >
+                                        <span className="label"></span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://github.com/BrockBatBlue"
+                                        className="icon brands bi-github"
+                                    >
+                                        <span className="label"></span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="mailto: emilybrockett@gmail.com"
+                                        class="icon solid bi-envelope"
+                                    >
+                                        <span className="label"></span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </footer>
+                    </div>
                 </div>
             </div>
         </section>
